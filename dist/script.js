e4ae294f052377799c757d9ed0f3aa8c87440794
arckhr21 <arcad.khrulev@gmail.com>
2021-05-27 11:28:50 +0300

    second tabs

--- dist/script.js
+++ dist/script.js
@@ -14917,8 +14917,9 @@ __webpack_require__.r(__webpack_exports__);
 var tabs = function tabs() {
   var glazingSlider = document.querySelector('.glazing_slider'),
       glazingBlock = document.querySelectorAll('.glazing_block'),
-      glazingContent = document.querySelectorAll('.row.glazing_content'); // console.log(glazingBlock);
-  // console.log(glazingContent);
+      glazingContent = document.querySelectorAll('.row.glazing_content'),
+      decorationItem = document.querySelectorAll('.decoration_item'),
+      decorationContent = document.querySelectorAll('.decoration_content .row');
 
   function hideGlazingContent(a) {
     for (var i = a; i < glazingContent.length; i++) {
@@ -14938,9 +14939,31 @@ var tabs = function tabs() {
   }
 
   ;
+
+  function hideDecorationContent(a) {
+    for (var i = a; i < glazingContent.length; i++) {
+      decorationContent[i].classList.remove('show');
+      decorationContent[i].classList.add('hide');
+    }
+  }
+
+  ;
+
+  function showDecorationContent(b) {
+    if (decorationContent[b].classList.contains('hide')) {
+      decorationContent[b].classList.remove('hide');
+      decorationContent[b].classList.add('show');
+    }
+  }
+
+  ;
   glazingSlider.addEventListener('click', function (e) {
     //получаем массив из 5-ти узлов glazing_block
     var target = e.target;
+
+    if (target) {
+      e.preventDefault(); // console.log(e.target);
+    }
 
     for (var i = 0; i < glazingBlock.length; i++) {
       if (target == glazingBlock[i].children[0] || target == glazingBlock[i].children[1] || target == glazingBlock[i]) {
@@ -14963,6 +14986,37 @@ var tabs = function tabs() {
     } //end for
 
   }); //end Listener
+  //табы отделки
+
+  var _loop = function _loop(i) {
+    decorationItem[i].addEventListener('click', function (e) {
+      var target = e.target;
+
+      if (target) {
+        e.preventDefault();
+      }
+
+      ;
+      decorationContent[0].children[0].classList.add('hide'); //подключение класса after_click
+
+      var j;
+
+      if (i != j) {
+        for (j = 0; j < decorationItem.length; j++) {
+          decorationItem[j].children[0].classList.remove('after_click');
+          decorationContent[0].children[j].classList.remove('show');
+        }
+
+        decorationItem[i].children[0].classList.add('after_click');
+        decorationContent[0].children[i].classList.add('show');
+      }
+    });
+  };
+
+  for (var i = 0; i < decorationItem.length; i++) {
+    _loop(i);
+  } //end for
+
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (tabs);
